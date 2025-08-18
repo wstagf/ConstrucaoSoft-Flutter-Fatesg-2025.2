d67f5bfb358d59ca29c1ca7f034770277a21e983
Thiago Augusto <wstagf@gmail.com>
2025-08-18 20:21:25 -0300

    Executado slide operadores aritimeticos

---
+++ Aula02/03-operadores-aritimeticos.dart
@@ -0,0 +1,13 @@
+void main() {
+  print("Operadores Aritméticos em Dart");
+
+  int a = 10;
+  int b = 3;
+
+  print("Soma: ${a + b}");
+  print("Subtração: ${a - b}");
+  print("Multiplicação: ${a * b}");
+  print("Divisão: ${a / b}");
+  print("Inteiro da divisao: ${a ~/ b}");
+  print("Módulo: ${a % b}");
+}
