ea7581b1cc1e61aa465d1aafa6925a5b481c2e53
Thiago Augusto <wstagf@gmail.com>
2025-08-18 20:21:38 -0300

    Executado slide operadores de comparacao

---
+++ Aula02/04-operador-comparacao.dart
@@ -0,0 +1,13 @@
+void main() {
+  print("Operadores de Comparação em Dart");
+
+  int a = 10;
+  int b = 3;
+
+  print("Igualdade: ${a == b}");
+  print("Diferença: ${a != b}");
+  print("Maior que: ${a > b}");
+  print("Menor que: ${a < b}");
+  print("Maior ou igual: ${a >= b}");
+  print("Menor ou igual: ${a <= b}");
+}
