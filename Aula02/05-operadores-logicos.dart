14972e40a231af241e83f6e2078739823479961e
Thiago Augusto <wstagf@gmail.com>
2025-08-18 21:08:32 -0300

    Criado codigo para o slide de operadores logicos

---
+++ Aula02/05-operadores-logicos.dart
@@ -0,0 +1,10 @@
+void main(List<String> args) {
+  print("operadores lógicos em Dart");
+  bool a = true;
+  bool b = false;
+
+  print("E: ${a && b}");
+  print("OU: ${a || b}");
+  print("NÃO: ${!a}");
+  print("NÃO: ${!b}");
+}
