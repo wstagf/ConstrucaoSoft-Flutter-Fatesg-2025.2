eb9e663def20a3cc7e5c3a419058bbc4c8a32907
Thiago Augusto <wstagf@gmail.com>
2025-09-08 21:48:22 -0300

    Implementado setstate

--- Aula05/combustivel_app/lib/home.dart
+++ Aula05/combustivel_app/lib/home.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class HomePage extends StatefulWidget {
+  HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String melhorOpcao = "-- Vamos Descobrir";
+  double valorGasolina = 0;
+  double valorAlcool = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -9,7 +18,7 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(title: Text("Vamos de Alcool ou Gasolina ")),
       body: Container(
         padding: EdgeInsets.all(25),
-        color: Colors.amber,
+        color: Colors.white,
         width: MediaQuery.of(context).size.width,
 
         height: MediaQuery.of(context).size.height,
@@ -17,22 +26,71 @@ class HomePage extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Container(
-              color: Colors.red,
-              width: 50,
-              height: 100,
-              child: Center(child: Text("Imagem")),
-            ),
+            // Container(
+            //   color: Colors.red,
+            //   width: 50,
+            //   height: 100,
+            //   child: Center(child: Text("Imagem")),
+            // ),
+            SizedBox(height: 10),
             Text(
               "Com base em algoritmos avançados aqui você descobre se é mais vantagem alcool ou gasolina, se bem que atualmente tanto faz, a gente so se...",
               style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
               textAlign: TextAlign.center,
             ),
-            Form(child: Column(children: [TextFormField(), TextFormField()])),
-            // FilledButton(onPressed: () {}, child: Text("aaa")),
+            SizedBox(height: 10),
+            Form(
+              child: Column(
+                children: [
+                  TextFormField(
+                    onChanged: (value) => {
+                      setState(() {
+                        valorGasolina = double.parse(value);
+                      }),
+                    },
+                    decoration: InputDecoration(
+                      labelText: "Valor da Gasolina em Reais. Ex. R\$ 5.98",
+
+                      fillColor: Colors.grey.withAlpha(150),
+                      filled: true,
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  TextFormField(
+                    onChanged: (value) => {
+                      setState(() {
+                        valorAlcool = double.parse(value);
+                      }),
+                    },
+
+                    decoration: InputDecoration(
+                      labelText: "Valor do Alcool em Reais. Ex. R\$ 5.98",
+                      fillColor: Colors.grey.withAlpha(150),
+                      filled: true,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ), // FilledButton(onPressed: () {}, child: Text("aaa")),
             InkWell(
               onTap: () {
-                print("clicou aqui");
+                setState(() {
+                  print("antes: " + melhorOpcao);
+
+                  print("clicou aqui");
+                  double divisao = valorAlcool / valorGasolina;
+
+                  if (divisao < 0.7) {
+                    melhorOpcao = "Alcool";
+                  } else {
+                    melhorOpcao = "Gasolina";
+                  }
+
+                  print("alterado: " + melhorOpcao);
+                });
               },
               child: Container(
                 padding: EdgeInsets.only(top: 10, bottom: 10),
@@ -45,7 +103,22 @@ class HomePage extends StatelessWidget {
                 child: Center(child: Text("Calcular")),
               ),
             ),
-            Text("É melhor abastecer de gasolina"),
+            SizedBox(height: 10),
+            Text(
+              "Alcool ${valorAlcool}",
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              textAlign: TextAlign.center,
+            ),
+            Text(
+              "Gasolina ${valorGasolina}",
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              textAlign: TextAlign.center,
+            ),
+            Text(
+              "É melhor abastecer com ${melhorOpcao}",
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              textAlign: TextAlign.center,
+            ),
           ],
         ),
       ),
