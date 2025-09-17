d21773f7b8d8bc4c2242a5681ea6e594691b4e39
Thiago Augusto <wstagf@gmail.com>
2025-09-17 20:25:23 -0300

    criado funcao para componentizar

--- Aula08/componentes/lib/home_page.dart
+++ Aula08/componentes/lib/home_page.dart
@@ -3,6 +3,26 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
+  Widget buildButton({
+    required String textoBotao,
+    required Color cor,
+    required void Function() quandoClicar,
+  }) {
+    return GestureDetector(
+      onTap: () {
+        quandoClicar();
+      },
+      child: Container(
+        width: 100,
+        decoration: BoxDecoration(
+          color: cor,
+          borderRadius: BorderRadius.all(Radius.circular(10)),
+        ),
+        child: Center(child: Text(textoBotao)),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,19 +32,26 @@ class HomePage extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            GestureDetector(
-              onTap: () {
-                print('cliqui');
+            buildButton(
+              textoBotao: "Texto 1",
+              cor: Colors.red,
+              quandoClicar: () {
+                print("1");
               },
-              child: Container(
-                height: 30,
-                width: 100,
-                decoration: BoxDecoration(
-                  color: Colors.blue,
-                  borderRadius: BorderRadius.all(Radius.circular(10)),
-                ),
-                child: Center(child: Text("Click Aqui")),
-              ),
+            ),
+            buildButton(
+              textoBotao: "Texto 2",
+              cor: Colors.blue,
+              quandoClicar: () {
+                print("2");
+              },
+            ),
+            buildButton(
+              textoBotao: "Texto 3",
+              cor: Colors.yellow,
+              quandoClicar: () {
+                print("3");
+              },
             ),
           ],
         ),
