d62efca2544a455974efed43c0dd163d5ad6bda0
Thiago Augusto <wstagf@gmail.com>
2025-09-22 19:51:21 -0300

    Copiado pagina de splash que ja possuimamos

---
+++ Aula09/images/lib/splash.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+import 'home_page.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  final GlobalKey<NavigatorState> aaaaNavigatorKey =
+      GlobalKey<NavigatorState>();
+
+  @override
+  void initState() {
+    super.initState();
+    print("Inicalizou");
+    navegar();
+  }
+
+  void navegar() {
+    Future.delayed(Duration(seconds: 5), () {
+      aaaaNavigatorKey.currentState?.pushReplacement(
+        MaterialPageRoute(builder: (context) => HomePage()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Titulo da janela",
+      color: Colors.red,
+      navigatorKey: aaaaNavigatorKey,
+
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        body: Container(
+          color: Colors.red,
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // Container(color: Colors.white, height: 100, width: 100),
+              // Container(height: 20),
+              // Container(color: Colors.blue, height: 100, width: 100),
+              FlutterLogo(size: 150),
+              Container(height: 20),
+              CircularProgressIndicator(color: Colors.white),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
