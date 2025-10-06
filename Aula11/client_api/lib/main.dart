2ea05c53fd9846958bf4c1b892fa6a182efa1c14
Thiago Augusto <wstagf@gmail.com>
2025-10-06 20:56:19 -0300

    retirado baner de debug

--- Aula11/client_api/lib/main.dart
+++ Aula11/client_api/lib/main.dart
@@ -16,6 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
+      debugShowCheckedModeBanner: false,
       home: ListaPage(),
     );
   }
