70d0aa3380ce24dfac12551dc643eb0ad83ba9d1
Thiago Augusto <wstagf@gmail.com>
2025-10-06 20:56:30 -0300

    implementado get na api

--- Aula11/client_api/lib/listaPage.dart
+++ Aula11/client_api/lib/listaPage.dart
@@ -1,4 +1,7 @@
+import 'dart:convert' as convert;
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class ListaPage extends StatefulWidget {
   const ListaPage({super.key});
@@ -8,8 +11,39 @@ class ListaPage extends StatefulWidget {
 }
 
 class _ListaPageState extends State<ListaPage> {
+  void carregarDados() async {
+    var response = await http.get(
+      Uri.parse("https://bible-api.com/Jo%C3%A3o+3:16?translation=almeida"),
+    );
+
+    print(response);
+
+    var jsonResponse =
+        convert.jsonDecode(response.body) as Map<String, dynamic>;
+
+    print(jsonResponse);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Minha Biblia"),
+        actions: [
+          GestureDetector(
+            onTap: () {
+              carregarDados();
+            },
+            child: Text("Carregar"),
+          ),
+          Container(
+            width: 20,
+          ),
+        ],
+      ),
+      body: Column(
+        children: [],
+      ),
+    );
   }
 }
