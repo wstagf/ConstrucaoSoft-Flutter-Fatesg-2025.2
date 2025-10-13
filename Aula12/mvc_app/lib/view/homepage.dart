d556491593c8d42841862c6361d39e73989a76ef
Thiago Augusto <wstagf@gmail.com>
2025-10-13 20:48:39 -0300

    Criado Projeto

---
+++ Aula12/mvc_app/lib/view/homepage.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  // criar um controler
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
