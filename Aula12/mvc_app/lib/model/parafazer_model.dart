6aaefd912d29951f86b7204af1e72d7f1d59c975
Thiago Augusto <wstagf@gmail.com>
2025-10-13 20:49:20 -0300

    criado model

---
+++ Aula12/mvc_app/lib/model/parafazer_model.dart
@@ -0,0 +1,13 @@
+class ParaFazer {
+  int? id;
+  String? todo;
+  bool? completed;
+  int? userId;
+
+  ParaFazer({
+    required this.id,
+    required this.todo,
+    required this.completed,
+    required this.userId,
+  });
+}
