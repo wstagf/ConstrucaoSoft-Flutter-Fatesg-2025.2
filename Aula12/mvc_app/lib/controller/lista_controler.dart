fe1232cff848368d4f2a466195054021277b3206
Thiago Augusto <wstagf@gmail.com>
2025-10-13 20:51:37 -0300

    Comencado a criar o controler

---
+++ Aula12/mvc_app/lib/controller/lista_controler.dart
@@ -0,0 +1,16 @@
+import 'package:mvc_app/model/parafazer_model.dart';
+
+class ListaControler {
+  List<ParaFazer> minhaLista = [];
+
+  // get all
+  List<ParaFazer> obterTodas() {
+    return minhaLista;
+  }
+
+  // get por id
+
+  // update por id
+
+  // delete
+}
