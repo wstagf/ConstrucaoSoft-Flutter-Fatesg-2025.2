d52e538b21dbb8c9f321dd24a05c37e065fd8b00
Thiago Augusto <wstagf@gmail.com>
2025-10-20 21:24:37 -0300

    colocando para exibit o mapa e a localizacao do usuario

--- Aula13/appmapas/lib/homePage.dart
+++ Aula13/appmapas/lib/homePage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_osm_plugin/flutter_osm_plugin.dart';
 import 'package:location/location.dart';
 
 class Homepage extends StatefulWidget {
@@ -9,7 +10,10 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage> {
-  LocationData? locationData;
+  LocationData locationData = LocationData.fromMap({
+    "latitude": 0.0,
+    "longitude": 0.0,
+  });
   Location location = Location();
 
   @override
@@ -57,18 +61,89 @@ class _HomepageState extends State<Homepage> {
                 "Minha localização é",
                 style: TextStyle(fontSize: 24),
               ),
-              locationData != null
-                  ? Text(
-                      "Lat: " + locationData!.latitude.toString(),
-                      style: TextStyle(fontSize: 24),
-                    )
-                  : Text(" "),
-              locationData != null
-                  ? Text(
-                      "Long: " + locationData!.longitude.toString(),
-                      style: TextStyle(fontSize: 24),
-                    )
-                  : Text(" "),
+              Text(
+                "Lat: " + locationData!.latitude.toString(),
+                style: TextStyle(fontSize: 24),
+              ),
+
+              Text(
+                "Long: " + locationData!.longitude.toString(),
+                style: TextStyle(fontSize: 24),
+              ),
+
+              Expanded(
+                child: Container(
+                  // ignore: sort_child_properties_last
+                  child: OSMFlutter(
+                    controller: MapController(
+                      initPosition: GeoPoint(
+                        latitude: locationData.longitude == null
+                            ? 0
+                            : locationData.longitude!,
+                        longitude: locationData.latitude == null
+                            ? 0
+                            : locationData.latitude!,
+                      ),
+                      areaLimit: const BoundingBox(
+                        east: 10.4922941,
+                        north: 47.8084648,
+                        south: 45.817995,
+                        west: 5.9559113,
+                      ),
+                    ),
+                    osmOption: OSMOption(
+                      // staticPoints: [
+                      //   StaticPositionGeoPoint(
+                      //     "10000",
+                      //     MarkerIcon(
+                      //       icon: Icon(
+                      //         Icons.location_on,
+                      //         color: Colors.blue,
+                      //         size: 48,
+                      //       ),
+                      //     ),
+                      //     [
+                      //       GeoPoint(
+                      //         latitude: -49.2537428,
+                      //         longitude: -16.6890524,
+                      //       ),
+                      //     ],
+                      //   ),
+                      // ],
+                      userTrackingOption: const UserTrackingOption(
+                        enableTracking: true,
+                        unFollowUser: false,
+                      ),
+                      zoomOption: const ZoomOption(
+                        initZoom: 8,
+                        minZoomLevel: 3,
+                        maxZoomLevel: 19,
+                        stepZoom: 1.0,
+                      ),
+                      userLocationMarker: UserLocationMaker(
+                        personMarker: const MarkerIcon(
+                          icon: Icon(
+                            Icons.location_history_rounded,
+                            color: Colors.red,
+                            size: 48,
+                          ),
+                        ),
+                        directionArrowMarker: const MarkerIcon(
+                          icon: Icon(
+                            Icons.location_on,
+                            size: 64,
+                          ),
+                        ),
+                      ),
+                      roadConfiguration: const RoadOption(
+                        roadColor: Colors.yellowAccent,
+                      ),
+                    ),
+                  ),
+                  width: double.infinity,
+                  color: Colors.red,
+                ),
+              ),
             ],
           ),
         ),
--- Aula13/appmapas/macos/Flutter/GeneratedPluginRegistrant.swift
+++ Aula13/appmapas/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,11 @@ import FlutterMacOS
 import Foundation
 
 import location
+import package_info_plus
+import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
